0d61548573754fe4b295c531ee0ff4af1e302390
eefreddy <emmanuelfreddy@gmail.com>
2020-03-27 00:54:54 -0400

    finish up about page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,7 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import pic01 from '../images/pic01.jpg'
-import pic02 from '../images/pic02.jpg'
-import pic03 from '../images/pic03.jpg'
+import toronto from '../images/toronto.jpg'
+import edmonton from '../images/edmonton.jpg'
 
 class Main extends React.Component {
   constructor(props){
@@ -41,9 +40,45 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Work</h2>
-          <span className="image main">
-            <img src={pic02} alt="" />
-          </span>
+          <div className="grid-container">
+            <div className="grid-item">
+              <span className="image fit fit-zero">
+                <img src={toronto} alt="" />
+                <h5>Software Engineer - Growth Toronto, Canada</h5>
+              </span>
+            </div>
+            <div className="grid-item article-text article-text-right">
+              <p>
+                As a member of the Growth team at Jobber,
+                I leverage data, cross functional experimentation and engineering to identify areas of high growth for the company.
+              </p>
+              <p>
+                On this team I've learnt I've learnt about the company's acquisition channels, growth levers and the bottlenecks.
+              </p>
+              <p>
+                In my role, I work with technologies like Rails, React, PostgreSQL and
+                Javascript; and I use Amplitude and Inspectlet to gain insight into user behaviour.
+              </p>
+            </div>
+            <div className="grid-item">
+              <span className="image fit fit-zero">
+                <img src={edmonton} alt="" />
+                <h5>Software Engineer | Edmonton, Canada</h5>
+              </span>
+            </div>
+            <div className="grid-item article-text article-text-right">
+              <p>
+                As an engineer on a scrum team,
+                I was responsible for making customer requirements into product features,
+                identifying improvable workflows and
+                build new features that are critical to business growth.
+              </p>
+              <p>
+                I learnt how to run scrum meetings, work as part of an agile team, iteratively build features and work with designers to understand the nuances of intuitive product design.
+              </p>
+            </div>
+          </div>
+
           <p>
           </p>
           <p>
@@ -60,7 +95,7 @@ class Main extends React.Component {
         >
           <h2 className="major">About</h2>
           <span className="image main">
-            <img src={pic03} alt="" />
+            <img src={toronto} alt="" />
           </span>
           <p>
           </p>
